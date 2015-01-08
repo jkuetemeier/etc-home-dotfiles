4923d8c5f6cd0bc87bf28e335dd2e5cc16534ccb
Jörg Kütemeier <jkuetemeier@jkuetemeier.com>
2015-01-08 13:37:34 +0100

    essential node modules

---
+++ node/setup.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+npm install -g grunt gulp
+npm install -g jshint
+npm install -g yo
+npm install -g mocha
+npm install -g http-server
+npm install -g bower
+npm install -g coffee coffeelint
+npm install -g less
+npm install -g node-inspector
+
